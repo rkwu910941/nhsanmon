7cf26fa35f169ef0b649fa73a416ca689f6ab675
rkwu910941 <rk.wu@msa.hinet.net>
2026-08-12 17:17:30 +0800

    20260812_1717

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,44 +1,50 @@
 <script setup lang="ts">
-const { loggedIn, fetch: refreshSession } = useUserSession()
-const form = reactive({
-  email: 'admin@admin.com',
-  password: 'iamtheadmin'
-})
-const loading = ref(false)
-const errorMessage = ref('')
-watchEffect(async () => {
-  if (loggedIn.value) {
-    await navigateTo('/backendsys/profile')
+  const { loggedIn, fetch: refreshSession } = useUserSession()
+
+  //響應式變數定義
+  const form = reactive({
+    email: '',
+    password: ''
+  })
+  const loading = ref(false)
+  const errorMessage = ref('')
+
+  //登入成功跳轉方法定義
+  watchEffect(async () => {
+    if (loggedIn.value) {
+      await navigateTo('/backendsys/profile')
+    }
+  })
+
+  //登入方法內容定義
+  const login = async () => {
+    errorMessage.value = ''
+    try {
+      loading.value = true
+      await $fetch('/api/login', {
+        method: 'POST',
+        body: form
+      })
+      await refreshSession()
+      await navigateTo('/backendsys/profile')
+    } catch (error: any) {
+      errorMessage.value =
+        error?.data?.statusMessage ||
+        error?.data?.message ||
+        '登入失敗'
+    } finally {
+      loading.value = false
+    }
   }
-})
-const login = async () => {
-  errorMessage.value = ''
-  try {
-    loading.value = true
-    await $fetch('/api/login', {
-      method: 'POST',
-      body: form
-    })
-    await refreshSession()
-    await navigateTo('/backendsys/profile')
-  } catch (error: any) {
-    errorMessage.value =
-      error?.data?.statusMessage ||
-      error?.data?.message ||
-      '登入失敗'
-  } finally {
-    loading.value = false
-  }
-}
 </script>
 
 <template>
   <div class="min-h-200 flex items-center justify-center p-6">
     <div class="w-full max-w-md">
-      <UCard>
+      <UCard class="dark:ring-white ring-black" :ui="{ header: 'border-black dark:border-white', body:'border-b border-black' , footer:'border-black'}">
         <template #header>
           <div class="text-center">
-            <h1 class="text-2xl font-bold">會員登入</h1>
+            <h1 class="text-2xl font-bold">後台登入</h1>
           </div>
         </template>
         <div class="space-y-4">
@@ -47,25 +53,28 @@ const login = async () => {
             color="error"
             variant="soft"
             title="登入失敗"
+            class="dark:bg-red-200"
             :description="errorMessage"
           />
           <div class="space-y-2">
-            <label class="text-sm font-medium">Email</label>
+            <label class="text-sm font-medium pr-8">Email</label>
             <UInput
               v-model="form.email"
               type="email"
               placeholder="請輸入 Email"
               size="lg"
+              :ui="{base: 'ring-black dark:ring-white w-70'}"
             />
           </div>
           <div class="space-y-2">
-            <label class="text-sm font-medium">Password</label>
+            <label class="text-sm font-medium pr-2">Password</label>
             <UInput
               v-model="form.password"
               type="password"
-              placeholder="請輸入密碼"
+              placeholder="請輸入密碼........"
               size="lg"
               @keyup.enter="login"
+              :ui="{base: 'ring-black dark:ring-white w-70'}"
             />
           </div>
           <UButton
@@ -74,10 +83,10 @@ const login = async () => {
             color="primary"
             :loading="loading"
             @click="login"
+            class="dark:text-black dark:bg-gray-400"
           >
             登入
           </UButton>
-
         </div>
       </UCard>
     </div>
